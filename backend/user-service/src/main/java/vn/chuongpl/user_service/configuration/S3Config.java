07ce44460713c1aeb0c8b615103c679d560c8b41
phamlucchuong <lucchuongg@gmail.com>
2026-06-14 13:53:56 +0700

    fix: add MinIO local dev support and fix S3 bucket-not-found error
    - Add AWS_S3_ENDPOINT_URL support to S3Config with pathStyleAccessEnabled
      so MinIO can be used as an S3 drop-in for local development
    - Add MinIO service and minio-init bucket-creation service to docker-compose

--- backend/user-service/src/main/java/vn/chuongpl/user_service/configuration/S3Config.java
+++ backend/user-service/src/main/java/vn/chuongpl/user_service/configuration/S3Config.java
@@ -7,7 +7,10 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.net.URI;
 
 @Configuration
 public class S3Config {
@@ -21,21 +24,32 @@ public class S3Config {
     @Value("${AWS_SECRET_ACCESS_KEY:}")
     String secretKey;
 
+    @Value("${AWS_S3_ENDPOINT_URL:}")
+    String endpointUrl;
+
     @Bean
     public S3Client s3Client() {
         AwsBasicCredentials creds = AwsBasicCredentials.create(accessKey, secretKey);
-        return S3Client.builder()
+        var builder = S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(creds))
-                .build();
+                .credentialsProvider(StaticCredentialsProvider.create(creds));
+        if (!endpointUrl.isBlank()) {
+            builder.endpointOverride(URI.create(endpointUrl))
+                    .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build());
+        }
+        return builder.build();
     }
 
     @Bean
     public S3Presigner s3Presigner() {
         AwsBasicCredentials creds = AwsBasicCredentials.create(accessKey, secretKey);
-        return S3Presigner.builder()
+        var builder = S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(creds))
-                .build();
+                .credentialsProvider(StaticCredentialsProvider.create(creds));
+        if (!endpointUrl.isBlank()) {
+            builder.endpointOverride(URI.create(endpointUrl))
+                    .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build());
+        }
+        return builder.build();
     }
 }
